c474486384c1f1cd6492d7db10b904803acb9109
Pattor <pa77or@gmail.com>
2017-12-17 03:36:27 -0500

    Cleaning some code. Changed logo design. Fixed grid problem with firefox

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 
   let width = $(self).outerWidth();
   $('#toggle-menu').on('click',function(){
+    $("#logo").toggleClass('toggle-animation');
     $('nav.nav-header').slideToggle('slow');
     $('footer').slideToggle('slow');
   });
@@ -21,51 +22,23 @@
   function CreatingTheGrid(windowWidth){
     let grid = $("#grid");
     let children = grid.children();
-
-    let c1=0;
-    let c2=0;
-    let c3=0;
-
     for (let i = 0; i < children.length; i++) {
-      var finalResultx = BuildingCard(children.eq(i));
-      if(i === children.length - 1){
-        let gridHeight = $("#grid").height();
-        let repetitions = Math.ceil(gridHeight / 10);/*10px*/
-        grid.css(`grid-template-rows`,`repeat(${repetitions},10px)`);
-        console.log("Height of the grid");
-        console.log($("#grid").height());
-      }
+      ///SETTING ALL THE HEIGHTS OF THE CARDS
+      BuildingCard(children.eq(i));
     }
   }
 
   function BuildingCard(card,i){
     let array = card.children();
     let height = 0;
-    console.log("Height per card");
-    console.log(array);
     for (var i = 0; i < array.length; i++) {
-      console.log("i: "+i);
-      console.log(array.eq(i).height());
-      //we are gonna add the margin
-      if(i === 1){//this is h2--margin 4%ofTheWidth
-        height += array.eq(i).height() + Math.ceil((array.eq(i).width() * 2)/100);
-      }
-      if(i === 2){//this is h2--margin 4%ofTheWidth
-        height += array.eq(i).height() + Math.ceil((array.eq(i).width() * 7)/100);
-      }
-      else{
-        height += array.eq(i).height();
-      }
+      height += array.eq(i).height();
+      //ADD THE MARGIN&&PADDING OF PARAGRAPHs,DIVs,HEADINGs
+      height += 10 + 12;
     }
     let repetitions = Math.ceil(height / 10);/*10px*/
     card.css(`grid-row-end`,`span ${repetitions}`);
-    return height;
   }
-
-  function isTheSmallestOne(v,sibling1,sibling2){
-    return v === Math.min(Math.min(v,sibling1),sibling2);
-  }
-
   function OrientResizeFunction(){
     if(width >= 550){
       CreatingTheGrid(width);
@@ -73,9 +46,7 @@
   }
 
   $(window).on('load', function(){
-
     console.log("The page load");
-    console.log(width);
     if(width >= 550){
     OrientResizeFunction();
     }
